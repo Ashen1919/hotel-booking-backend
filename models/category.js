b8054a9bf57befc1d2200847073c5d2185ddd221
Ashen1919 <2303S0834@saegis.ac.lk>
2024-10-12 16:57:28 +0530

    created categorySchema

---
+++ models/category.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const categorySchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : String,
+        required : true
+    }
+})
+
+const Category = mongoose.model("category", categorySchema);
+
+export default Category;
