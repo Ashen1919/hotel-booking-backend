855bf2e8c810e14a48da53ea18c8b88cb7ad07c0
Ashen1919 <2303S0834@saegis.ac.lk>
2024-11-27 16:05:08 +0530

    modify

--- models/user.js
+++ models/user.js
@@ -39,7 +39,7 @@ const userSchema = mongoose.Schema({
     },
     profileImage : {
         type: String,
-        default: ""
+        default: "https://cloud.appwrite.io/v1/storage/buckets/672a1e700037c646954e/files/674578c80028b7645a44/view?project=672a1dc2000b4396bb7d&project=672a1dc2000b4396bb7d&mode=admin"
     }
     
 })
